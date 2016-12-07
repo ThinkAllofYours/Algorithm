7ab2c2fccad27531c5590d35e37190c801bf89cb
DESKTOP-P61SS5F\lyder <e.where.people.enjoy@gmail.com>
2016-12-07 23:55:33 +0900

    LeetCode 55. Jump Game - Solution made by C#

---
+++ C_Sharp/leetcode/55.JumpGame/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jumpNum
+{
+    public class Solution
+    {
+        public bool CanJump(int[] nums)
+        {
+            bool isCanJump = false;
+            int maximum = 0;
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                int farthest = i + nums[i];
+
+                if (farthest > maximum)
+                    maximum = farthest;
+
+                if (maximum >= nums.Length - 1)
+                    return isCanJump = true;
+
+                if (maximum <= i)
+                    return isCanJump;
+            }
+            return isCanJump;
+        }
+    }
+}
