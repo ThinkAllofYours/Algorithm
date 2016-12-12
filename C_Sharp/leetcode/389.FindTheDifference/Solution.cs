5b6aa4c5398944a1424154cc778691e37f6c6d77
DESKTOP-P61SS5F\lyder <e.where.people.enjoy@gmail.com>
2016-12-12 14:08:57 +0900

    leetCode 389. FindTheDifference c#

---
+++ C_Sharp/leetcode/389.FindTheDifference/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheDifference
+{
+    public class Solution
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            char answer = '0';
+            Dictionary<char, int> _dic = new Dictionary<char, int>();
+            foreach (char c in t.ToCharArray())
+            {
+                if (_dic.ContainsKey(c))
+                {
+                    int value = 0;
+                    _dic.TryGetValue(c, out value);
+                    //_dic.Add(c, value++); 키가 있으면 키를 더하는게 아니라 값을 증가시킨다.
+                    _dic[c] = ++value; // 증가한 값을 더하기 위해서 value++ 아니라 ++value를 시킨다.
+                }
+                else
+                {
+                    _dic.Add(c, 1);
+                }
+            }
+
+            foreach (char c in s.ToCharArray())
+            {
+                if (_dic.ContainsKey(c))
+                {
+                    int value = 0;
+                    _dic.TryGetValue(c, out value);
+                    _dic[c] = --value;
+                    if (value == 0)
+                    {
+                        _dic.Remove(c);
+                    }
+                }
+            }
+
+            foreach (char c in _dic.Keys)
+            {
+                answer = c;
+            }
+
+            return answer;
+        }
+    }
+}
